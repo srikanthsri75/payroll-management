a1ebcac5ec8d8bf35c940a80ae6412ee1ebcbc78
Daniel kamau nganga <danielkamaunganga3@gmail.com>
2025-11-24 22:06:42 +0300

    Update

--- backend/run.py
+++
@@ -1,9 +0,0 @@
-from app import create_app, db
-
-app = create_app()
-
-if __name__ == "__main__":
-    with app.app_context():
-        # create tables if not present (use migrations in prod)
-        db.create_all()
-    app.run(host="0.0.0.0", port=5000, debug=True)
